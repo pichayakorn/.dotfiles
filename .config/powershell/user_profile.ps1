2c773befe5e3d1d1388e8340eae510a244e053f0
Hachi <82988679+pichayakorn@users.noreply.github.com>
2023-10-20 15:56:46 +0900

    feat(pwsh): initial commit

---
+++ .config/powershell/user_profile.ps1
@@ -0,0 +1,49 @@
+# set PowerShell to UTF-8
+[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+
+Import-Module posh-git
+$omp_config = Join-Path $PSScriptRoot ".\hachi.omp.json"
+oh-my-posh init pwsh --config $omp_config | Invoke-Expression
+
+Import-Module -Name Terminal-Icons
+
+# PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+
+# Fzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Env
+$env:GIT_SSH = "C:\Windows\system32\OpenSSH\ssh.exe"
+
+# Alias
+Set-Alias -Name vim -Value nvim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+Set-Alias code codium
+Set-Alias exp explorer
+Set-Alias np notepad
+Set-Alias gpg 'C:\Program Files\Git\usr\bin\gpg.exe'
+
+# Utilities
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+
+# Python Virtualenv
+Set-Alias venv virtualenv
+function workon ($venv) {
+  if ( Test-Path -Path $venv) {
+    . $venv\Scripts\activate
+  } else {
+    echo "No venv attach, or wrong venv's name"
+  }
+}
